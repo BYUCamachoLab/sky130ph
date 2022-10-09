925f5ef7d88b37619ef2177f7dfed13d8409d2c0
SkandanC <s39chand@edu.uwaterloo.ca>
2022-10-09 18:05:27 -0400

    Update __init__.py

--- sky130ph/__init__.py
+++ sky130ph/__init__.py
@@ -1,4 +1,7 @@
-"""sky130ph - Skywater photonics PDK for the 130nm fab foundry"""
+"""sky130ph - Skywater photonics PDK for the 130nm fab foundry.add()
+
+TODO Create LayerStack, component library, and activate it here.
+"""
 
 __version__ = '0.0.1'
 __author__ = 'SkandanC <s39chand@uwaterloo.ca>'
