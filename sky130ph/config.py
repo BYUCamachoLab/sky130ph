1dca4ff8368265998e159e36f88b55d9bc91eef8
Skandan Chandrasekar <s39chand@edu.uwaterloo.ca>
2022-12-12 13:17:42 -0500

    Update config.py

--- sky130ph/config.py
+++ sky130ph/config.py
@@ -8,9 +8,6 @@ home = pathlib.Path.home()
 cwd = pathlib.Path.cwd()
 cwd_config = cwd / "config.yml"
 
-home_config = home / ".config" / "sky130ph.yml"
-config_dir = home / ".config"
-config_dir.mkdir(exist_ok=True)
 module_path = pathlib.Path(__file__).parent.absolute()
 repo_path = module_path.parent
 
